369363e2f1c3f0b90903d6f495fcc8165b3e3ae1
jfkeci <ja.lip132@gmail.com>
2021-07-30 08:25:41 +0200

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $notes = DB::select('SELECT * FROM notes WHERE user_id = ' . Auth()->user()->id);
+        return view('home')->with('notes', $notes);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        <a href="/notes/create" class="btn btn-primary">Create Note</a>
+                        <hr>
+                        <h3>Your Notes</h3>
+
+                        @if (count($notes) > 0)
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Title</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                @foreach ($notes as $note)
+                                    <tr>
+                                        <th>{{ $note->title }}</th>
+                                        <th><a href="/notes/{{ $note->id }}/edit" class="btn btn-default">Edit</a></th>
+                                        <th>
+                                            {!! Form::open(['action' => ['App\Http\Controllers\NotesController@destroy', $note->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+                                            {{ Form::hidden('_method', 'DELETE') }}
+                                            {{ Form::submit('Delete', ['class' => 'btn-btn-danger']) }}
+                                            {!! Form::close() !!}
+                                        </th>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <p>You have no notes</p>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
